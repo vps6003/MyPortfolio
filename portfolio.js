391081590d41419cd24ebecb294cc76576abb64b
vps6003 <79104766+vps6003@users.noreply.github.com>
2021-06-19 14:52:06 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,52 @@
+
+
+function show_hide1(){
+
+    let a = document.getElementById('School');
+    if(a.style.display=='none')
+    {
+        a.style.display='inline-block';
+    }
+    else
+    {
+        a.style.display='none';
+
+    }
+}
+
+
+function show_hide2(){
+
+    let a = document.getElementById('Jee');
+    
+    if(a.style.display=='none')
+    {
+        a.style.display='inline-block';
+    }
+    else
+    {
+        a.style.display='none';
+
+    }
+}
+
+
+function show_hide3(){
+
+    let a = document.getElementById('coding');
+    if(a.style.display=='none')
+    {
+        a.style.display='inline-block';
+    }
+    else
+    {
+        a.style.display='none';
+
+    }
+}
+
+function Myfunction(){
+
+var a = document.getElementById("scroll")
+
+}
